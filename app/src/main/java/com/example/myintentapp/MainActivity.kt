a0bea3cb83e71b0f618c2cba8ed2a21f3b3eba0f
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-03-06 23:56:48 +0700

    add feature explicit intent with data

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -13,6 +13,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveActivityWithData: Button = findViewById(R.id.btn_move_activity_with_data)
+        btnMoveActivityWithData.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -20,6 +23,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_with_data -> {
+                val moveActivityWithData =
+                    Intent(this@MainActivity, MoveActivityWithData::class.java)
+                moveActivityWithData.putExtra(MoveActivityWithData.EXTRA_NAME, "Faiz Rofi Hencya")
+                moveActivityWithData.putExtra(MoveActivityWithData.EXTRA_AGE, 21)
+                startActivity(moveActivityWithData)
             }
         }
     }
