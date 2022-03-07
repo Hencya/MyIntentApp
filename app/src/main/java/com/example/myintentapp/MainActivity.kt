f6abe511420c7bf97bce7f06f8d47442d55ede3e
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-03-07 12:20:15 +0700

    add feature intent launcher

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -5,9 +5,23 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvResult: TextView
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == MoveForResult.RESULT_CODE && result.data != null) {
+            val selectedValue = result.data?.getIntExtra(MoveForResult.EXTRA_SELECTED_VALUE, 0)
+            tvResult.text = "Hasil : $selectedValue"
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +37,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnDialNumber: Button = findViewById(R.id.btn_dial_number)
         btnDialNumber.setOnClickListener(this)
+
+        val btnMoveForResult: Button = findViewById(R.id.btn_move_activity_result)
+        btnMoveForResult.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
     }
 
     override fun onClick(v: View?) {
@@ -53,9 +72,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btn_dial_number -> {
                 val phoneNumber = "0899999999"
-                val dialPhoneintent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: $phoneNumber"))
-                startActivity(dialPhoneintent)
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: $phoneNumber"))
+                startActivity(dialPhoneIntent)
 
+            }
+            R.id.btn_move_activity_result -> {
+                val moveForResult = Intent(this@MainActivity, MoveForResult::class.java)
+                resultLauncher.launch(moveForResult)
             }
         }
     }
